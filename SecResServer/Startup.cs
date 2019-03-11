6fb19c7b1bd188047f20b40317bc8a2524e5d39a
Volodymyr Tytenko <tvolodi@hotmail.com>
2019-03-11 07:13:44 +0600

    SignalR is added

--- SecResServer/Startup.cs
+++ SecResServer/Startup.cs
@@ -5,25 +5,53 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SecResServer.Hangfire;
+using Hangfire.PostgreSql;
+using Hangfire;
+using SecResServer.Hubs;
 
 namespace SecResServer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HfDbContext>(options => 
+                options.UseNpgsql(Configuration.GetConnectionString("HfDbConnection")));
+
+            services.AddHangfire(configuration => configuration.UsePostgreSqlStorage(Configuration.GetConnectionString("HfDbConnection")));
+
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<SecHub>("/secHub");
+            });
 
             app.Run(async (context) =>
             {
