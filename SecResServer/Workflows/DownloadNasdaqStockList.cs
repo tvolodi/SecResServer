cd6df19b0e7e87c2d44275e7e219d6e1c0bfde69
Volodymyr Tytenko <tvolodi@hotmail.com>
2019-03-14 07:25:20 +0600

    Save work

---
+++ SecResServer/Workflows/DownloadNasdaqStockList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace SecResServer.Workflows
+{
+    public class DownloadNasdaqStockList : StepBody
+    {
+        public override ExecutionResult Run(IStepExecutionContext context)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile("https://www.nasdaq.com/screening/companies-by-name.aspx?letter=0&exchange=nasdaq&render=download", "nasdaq.csv");
+
+                return ExecutionResult.Next();
+            } catch (Exception e)
+            {
+                return ExecutionResult.Sleep(TimeSpan.FromSeconds(60), new object());
+            }
+        }
+    }
+}
